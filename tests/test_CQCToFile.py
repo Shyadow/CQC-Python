1a0e11dda665d76a3136f864a03a8f13e4924de3
Liam <11602015+Shyadow@users.noreply.github.com>
2019-10-21 21:33:48 +0200

    Two very simple tests for CQCToFile

--- tests/test_CQCToFile.py
+++ tests/test_CQCToFile.py
@@ -1,2 +1,31 @@
 # Tests for the CQCToFile class
 
+# To get a temporary directory start the function with
+#
+# filename=os.path.join(tmpdir,'CQC_File')
+#
+# with CQCToFile(filename=filename) as cqc:
+
+
+
+from cqc.pythonLib import CQCToFile, qubit
+import os
+
+def test_name(tmpdir):
+
+    filename=os.path.join(tmpdir,'CQC_File')
+
+    with CQCToFile(filename=filename) as cqc:
+        assert cqc.name == 'CQCToFile'
+
+def test_tempdir(tmpdir):
+
+    filename=os.path.join(tmpdir,'CQC_File')
+
+    with CQCToFile(filename=filename) as cqc:
+
+        cqc.commit('test')        
+        
+        with open(filename) as f:
+            contents = f.read()
+            assert contents == 'test\n'
