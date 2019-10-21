075d59a9e938241f7ac962ac74273440c0f3af8d
Liam <11602015+Shyadow@users.noreply.github.com>
2019-10-21 21:55:55 +0200

    Add test for output when creating a qubit

--- tests/test_CQCToFile.py
+++ tests/test_CQCToFile.py
@@ -10,6 +10,7 @@
 
 from cqc.pythonLib import CQCToFile, qubit
 import os
+from cqc.cqcHeader import CQC_TP_HELLO
 
 def test_name(tmpdir):
 
@@ -29,3 +30,32 @@ def test_tempdir(tmpdir):
         with open(filename) as f:
             contents = f.read()
             assert contents == 'test\n'
+
+def test_sendSimple(tmpdir):
+
+    filename=os.path.join(tmpdir,'CQC_File')
+
+    with CQCToFile(filename=filename) as cqc:
+
+        cqc.sendSimple(CQC_TP_HELLO)
+
+        with open(filename) as f:
+            contents = f.read()
+            print(contents)
+            assert contents[6:10] == "\\x00"
+
+def test_createqubit(tmpdir):
+
+    filename=os.path.join(tmpdir,'CQC_File')
+
+    with CQCToFile(filename=filename) as cqc:
+
+        q = qubit(cqc)
+
+        with open(filename) as f:
+            
+            line = f.readline()
+            print(line)
+
+            assert line[6:10] == "\\x01"
+            assert line[42:46] == "\\x01"
